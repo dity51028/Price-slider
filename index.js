98e5e129ab2fec10aec91d15717863c6434d92a1
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-06-26 20:35:54 +0530

    input field done

--- index.js
+++ index.js
@@ -5,25 +5,49 @@ progress =document.querySelector(".slider .progress");
 let priceGap = 1000;
 
 
-rangeInput.forEach(input=>{
+priceInput.forEach(input =>{
     input.addEventListener("input",e=>{
+        let minVal = parseInt(priceInput[0].value),
+        maxVal = parseInt(priceInput[1].value);
+         
+        if((maxVal-minVal>=priceGap) && maxVal <= 10000){
+            if(e.target.classname==="input-min")
+            {
+            rangeInput[0].value = minVal;
+            progress.style.left = (minVal/rangeInput[0].max)*100+"%";
+            }else{
+            rangeInput[1].value = maxVal; 
+            progress.style.right = 100-(maxVal/rangeInput[0].max)*100+"%";
+            }
+
+        }
+       
+    });
+});
+
+
+rangeInput.forEach(input =>{
+    input.addEventListener("input",(e)=>{
         let minVal = parseInt(rangeInput[0].value),
         maxVal = parseInt(rangeInput[1].value);
-
-        if(maxVal- minVal < priceGap){
-            if(e.target.classname === "range-min"){
-                rangeInput[0].value = maxVal - priceGap;
-                progress.style.left = (minVal/rangeInput[0].max)*100 + "%";
+         
+        if(maxVal-minVal<priceGap){
+            if(e.target.classname==="range-min")
+            {
+            rangeInput[0].value = maxVal-priceGap;
             }else{
-                rangeInput[1].value = maxVal + priceGap;
-                progress.style.right = 100-(maxVal/rangeInput[1].max)*100 + "%";
+            rangeInput[1].value = minVal+priceGap; 
             }
-            
-        }else{
-        priceInput[0].value = minVal;
-        priceInput[1].value = maxVal;
-        
-        }
 
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+
+            progress.style.left = (minVal/rangeInput[0].max)*100+"%";
+            progress.style.right = 100-(maxVal/rangeInput[0].max)*100+"%";
+        }
+       
+
+        console.log(percent);
     });
 });
